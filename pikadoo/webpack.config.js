c05a15cdf8d615a82cdc15e17ac9cbaa10865859
Tomislav Kravaršćan <tomislav.kravarscan@fer.hr>
2018-05-18 00:45:46 +0200

    Krenuli smo s projektom

--- pikadoo/webpack.config.js
+++ pikadoo/webpack.config.js
@@ -20,6 +20,27 @@ module.exports = {
                     presets: ['react', 'es2015', 'stage-3']
                 }
             }
+        ],
+        rules: [
+             {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-3']
+                }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }
+                ]
+            }
         ]
     },
     plugins: [new HtmlWebpackPlugin({
