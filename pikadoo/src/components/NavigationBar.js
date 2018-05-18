f0edd0b118210ed4ab991339e547f7c9242770ae
Tomislav Kravaršćan <tomislav.kravarscan@fer.hr>
2018-05-18 19:44:05 +0200

    Radi renderanje i login

--- pikadoo/src/components/NavigationBar.js
+++ pikadoo/src/components/NavigationBar.js
@@ -4,52 +4,49 @@ import { connect } from 'react-redux';
 
 
 class NavigationBar extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            sessionUser : ''
-        }
-    }
 
     render() {
-        const { user,loggedIn } = this.props;
+        const { user, loggedIn } = this.props;
 
         return (
             <nav className="navbar fixed-top navbar-inverse bg-dark">
-                    <div className="container-fluid">
-                        <div className="navbar-header">
-                            <a className="navbar-brand" href="#">Pikadoo</a>
-                        </div>
-                        <ul className="nav navbar-nav">
-                        <li><a href="#"><span className="text-white">{this.state.sessionUser}</span></a></li>
-                        </ul>
-
-                        <ul className="nav navbar-nav">
-                            <li><a href="/singleplayer"><span className="text-white">Single-player</span></a></li>
-                        </ul>
-
-                        <ul className="nav navbar-nav">
-                            <li><a href="#"><span className="text-white">Multi-player</span></a></li>
-                        </ul>
-                        {
-                            loggedIn && 
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">Pikadoo</a>
+                    </div>
+                    {
+                        loggedIn &&
+                        <div>
                             <ul className="nav navbar-nav">
-                                <li><a href="/user/{ user }"><span className="text-white">{ user }</span></a></li>
+                                <li><a href="/singleplayer"><span className="text-white">Single-player</span></a></li>
                             </ul>
-                        }
+                            <ul className="nav navbar-nav">
+                                <li><a href="#"><span className="text-white">Multi-player</span></a></li>
+                            </ul>
+                            <ul className="nav navbar-nav">
+                                <li><a href="/profile"><span className="text-white">Hello, {user.username}!</span></a></li>
+                            </ul>
+                            <ul className="nav navbar-nav navbar-right">
+                                <li><Link to="/login"><span className="glyphicon glyphicon-log-in" href="/login"></span> Logout</Link></li>
+                            </ul>
+                        </div>
+                    }
+                    {
+                        !loggedIn &&
                         <ul className="nav navbar-nav navbar-right">
-                            <li><Link to="/signup"><span className="glyphicon glyphicon-user" href="/singup"></span> Sign Up</Link></li>
+                            <li><Link to="/signup"><span className="glyphicon glyphicon-user" href="/register"></span> Sign Up</Link></li>
                             <li><Link to="/login"><span className="glyphicon glyphicon-log-in" href="/login"></span> Login</Link></li>
                         </ul>
-                    </div>
+                    }
+                </div>
             </nav>
         );
     }
 }
 
 function mapStateToProps(state) {
-    const { authentication, loggedIn } = state;
-    const { user } = authentication;
+    const { authentication } = state;
+    const { user, loggedIn } = authentication;
     return {
         user,
         loggedIn
