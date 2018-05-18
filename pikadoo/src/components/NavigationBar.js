3beb49aecfe2e786ca4627d87ba3ea7c1375cfff
Tomislav Kravaršćan <tomislav.kravarscan@fer.hr>
2018-05-18 18:36:11 +0200

    pokusaj

--- pikadoo/src/components/NavigationBar.js
+++ pikadoo/src/components/NavigationBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 class NavigationBar extends React.Component {
     constructor(props){
@@ -10,6 +12,8 @@ class NavigationBar extends React.Component {
     }
 
     render() {
+        const { user,loggedIn } = this.props;
+
         return (
             <nav className="navbar fixed-top navbar-inverse bg-dark">
                     <div className="container-fluid">
@@ -27,6 +31,12 @@ class NavigationBar extends React.Component {
                         <ul className="nav navbar-nav">
                             <li><a href="#"><span className="text-white">Multi-player</span></a></li>
                         </ul>
+                        {
+                            loggedIn && 
+                            <ul className="nav navbar-nav">
+                                <li><a href="/user/{ user }"><span className="text-white">{ user }</span></a></li>
+                            </ul>
+                        }
                         <ul className="nav navbar-nav navbar-right">
                             <li><Link to="/signup"><span className="glyphicon glyphicon-user" href="/singup"></span> Sign Up</Link></li>
                             <li><Link to="/login"><span className="glyphicon glyphicon-log-in" href="/login"></span> Login</Link></li>
@@ -37,4 +47,14 @@ class NavigationBar extends React.Component {
     }
 }
 
-export default NavigationBar;
+function mapStateToProps(state) {
+    const { authentication, loggedIn } = state;
+    const { user } = authentication;
+    return {
+        user,
+        loggedIn
+    };
+}
+
+const connectedNavigationBar = connect(mapStateToProps)(NavigationBar);
+export { connectedNavigationBar as NavigationBar };
