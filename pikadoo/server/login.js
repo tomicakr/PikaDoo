971db5f9e0251d86664524f5aa0ffe5ad7d14fa4
Tomislav Kravaršćan <tomislav.kravarscan@fer.hr>
2018-05-19 11:13:58 +0200

    Server kopiran i promijenjena naredba

---
+++ pikadoo/server/login.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongojs = require('mongojs');
+
+const router = express.Router();
+router.use(require('express-validator')());
+
+var db = mongojs('pikadoo', ['users']);
+
+router.post('/login', (req, res) => {
+    const user = req.body.user;
+    req.checkBody('user.username', 'Username cannot be empty').notEmpty();
+    req.checkBody('user.password', 'Password cannot be empty').notEmpty();
+    let errors = req.validationErrors();
+
+    if(errors) {
+        res.json({errorsExist: true, errors: errors});
+        return;
+    }
+    
+    let userForSession = { username: user.username, email: user.email ,password: user.password}
+    
+    db.users.findOne({
+        $or:[{username: user.username, password: user.password}, {email: user.username, password: user.password}],
+    }, function(err, doc) {
+        if(err){
+            console.log(err);
+        } 
+
+        if(doc){
+            res.status(200).json({
+                errorsExist:false
+            })
+            req.session.sessionUser = userForSession;
+
+            return;
+        }
+
+        res.json({errorsExist: true, errors: [{msg: "Korisnik ili lozinka pogrešni"}]});
+    })
+    
+});
+
+module.exports = router;
---
+++ pikadoo/server/signup.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const mongojs = require('mongojs');
+
+const router = express.Router();
+router.use(require('express-validator')());
+
+var db = mongojs('pikadoo', ['users']);
+
+router.post('/signup', (req, res) => {
+    const user = req.body.user;
+    req.checkBody('user.email', 'Invalid email').isEmail();
+    req.checkBody('user.username', 'Username cannot be empty').notEmpty();
+    req.checkBody('user.password', 'Password cannot be empty').notEmpty();
+    req.checkBody('user.passwordC', 'Passwords do not match').equals(user.password);
+
+    let errors = req.validationErrors();
+
+    
+    if(errors) {
+        res.json({errorsExist: true, errors: errors});
+        return;
+    }
+    
+    let userToSave = { username: user.username, email: user.email ,password: user.password}
+    
+    db.users.findOne({
+        $or:[{username: userToSave.username}, {email: userToSave.email}]
+    }, function(err, doc) {
+        if(err){
+            console.log(err);
+        } 
+
+        console.log(doc);
+        if(doc){
+            res.json({errorsExist: true, errors: [{msg: "Korisnik već postoji"}]});
+            
+            return;
+        }
+
+        db.users.insert(userToSave);
+        res.status(200).json({
+            errorsExist:false
+        })
+    })
+    
+});
+
+module.exports = router;
