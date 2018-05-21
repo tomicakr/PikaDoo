475b79bb55f1d4379a2fe6f562327920573d8c90
Tomislav Kravaršćan <tomislav.kravarscan@fer.hr>
2018-05-21 19:03:25 +0200

    Malo detalja o gameovima na profil

--- pikadoo/server/profile.js
+++ pikadoo/server/profile.js
@@ -3,31 +3,30 @@ const mongojs = require('mongojs');
 
 const router = express.Router();
 
-var db = mongojs('pikadoo', ['users','games']);
+var db = mongojs('pikadoo', ['users', 'games']);
 
 router.get('/', (req, res) => {
     const username = req.query.username;
 
     db.users.findOne({
         username: username
-    }, function(err, doc) {
-        if(err){
-            res.json({errorExists: true});
+    }, function (err, doc) {
+        if (err) {
+            res.json({ errorExists: true });
             return;
         }
 
-        db.games.find({ user: username }, function (err, docs) { 
-            if(err){
+        db.games.find({ user: username }, function (err, docs) {
+            if (err) {
                 return;
             }
-            // let tosend = {
-            //     username: doc.username,
-            //     email: doc.email,
-            //     games: docs
-            // }
+            let tosend = {
+                username: doc.username,
+                email: doc.email,
+                games: docs
+            }
 
-            // res.json(tosend);
-            res.json({...doc ,games : docs});
+            res.json(tosend);
         });
     })
 });
--- pikadoo/src/components/Profile.jsx
+++ pikadoo/src/components/Profile.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { userActions } from '../_actions';
 
 class Profile extends React.Component {
-    componentDidMount () {
+    componentDidMount() {
         console.log("uslooooo");
         // zato što ako se ovo zove /profile, onda se i na backendu mora zvat /profile,
         // a onda ako upisemo /profile rucno u browser napravit ce get zahjev na to i nece prikazat
@@ -36,15 +36,50 @@ class Profile extends React.Component {
         const { username, email, games } = this.state;
         return (
             <div className="col-md-6 col-md-offset-3">
-                <h1>Username: {username}</h1>
-                <h1>Email: {email}</h1>
+
+                <h1>{username}</h1>
+                <h2><a href={"mailto:" + email}>{email}</a></h2>
+
+                <br/><br/><br/>
 
                 {
-                    games.map((game, index) => 
-                        <h2 key={index}>{game.user}</h2>
-                    )
+                    games.length !== 0 &&
+
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Number</th>
+                                <th>Type</th>
+                                <th>Mode</th>
+                                <th>Number Of Players</th>
+                                <th>Winner</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                games.map((game, index) => {
+                                    return (
+                                        <tr key={index}>
+                                            <td>{index}</td>
+                                            <td>{game.gameType}</td>
+                                            <td>{game.mode}</td>
+                                            <td>{game.players.length}</td>
+                                            <td>{game.winner}</td>
+                                            <td><button className="btn btn-small">Game Details</button></td>
+                                        </tr>
+                                    );
+                                }
+                                )
+                            }
+                        </tbody>
+                    </table>
+                }
+                {
+                    games.length === 0 &&
+                    <p>User hasn't been involved in any games yet.</p>
                 }
             </div>
+
         );
     }
 }
